c7794d873ce66c2cc9bb5d8d66c29316f88e0822
Reza <emailrezahere@gmail.com>
2023-07-29 22:23:49 +0100

    Remove unused libraries

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,6 @@ module com.phpswitcher.phpswitcher {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires com.dlsc.formsfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-
     opens com.phpswitcher.phpswitcher to javafx.fxml;
     exports com.phpswitcher.phpswitcher;
 }
